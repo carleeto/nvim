99b25bb87d890290705ee8e77ed89a882c157ce1
Carl Menezes <carleeto@gmail.com>
2025-01-14 11:53:20 +1300

    initial config

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,36 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "gopls", "terraformls" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+lspconfig.gopls.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern(".git", "go.mod","go.work"),
+      settings = {
+         gopls = {
+            gofumpt = true,
+            staticcheck = true,
+            usePlaceholders = true,
+         },
+      },
+}
